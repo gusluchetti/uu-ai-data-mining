7da7c6b740c89017bf928ef5615a15a46375e9a2
Gustavo Luchetti <gustavo@luchetti.dev>
2022-09-22 00:34:39 +0200

    feat: finish bestsplit func

--- gini.py
+++ gini.py
@@ -2,16 +2,14 @@ import numpy as np
 
 
 def gini_impurity(array):
+    print(array)
     # P(i) * (1 - P(i))
     length = len(array)
+    if length == 1:
+        return 0.0
     count_0 = np.count_nonzero(array == 0)
     p0 = count_0/length
     p1 = 1 - count_0/length
-    # print(f"p0 = {p0}; p1 = {p1};")
-    return p0*p1
-
-
-array = np.array([1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0])
-expected_gini = 0.23140495867768596
-gini = gini_impurity(array)
-assert gini == expected_gini
+    gini = p0 * p1
+    print(f"Gini = {gini}")
+    return gini
