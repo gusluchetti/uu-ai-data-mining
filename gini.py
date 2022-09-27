feat: fix gini formula for multi class

gini.py
@@ -6,11 +6,11 @@ import math
 def get_max_impurity(array):
     """array of unique elements"""
     length = len(array)
-    return math.pow(1/length, length)
+    return 1-length*(math.pow(1/length, 2))
 
 
 def gini_impurity(array):
-    # P(i) * (1 - P(i))
+    # 1 - sum (p)^2
     """ updated version of gini impurity to handle separation
     between a number N of different classes
     classe could either be numbers or chars
@@ -28,7 +28,10 @@ def gini_impurity(array):
         freq_p = count/length
         freqs = np.append(freqs, [freq_p])
     print(f"Freq. for each class -> {freqs}")
-    gini = np.prod(freqs)
+    sum = 0
+    for freq in freqs:
+        sum += math.pow(freq, 2)
+    gini = 1 - sum
     if freqs[0] == 1:  # for single element nodes
         gini = 0.0
     print(f"Gini = {gini}\n")

test.py
@@ -1,23 +1,26 @@
 import bestsplit
 import gini
 import numpy as np
-
+from math import pow
 
 # test suite for different functions being used
 def test_gini():
-    # asserting char array with max impurity
-    array = np.array([0, 1])
-    expected_gini = 0.25  # 1/2 * 1/2
+    # asserting binary array with given answer
+    array = np.array([1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0])  # 4 / 7
+    expected_gini = 0.23140495867768596 * 2
+    print(f"expected gini -> {expected_gini}")
     assert gini.gini_impurity(array) == expected_gini
 
-    # asserting binary array with given answer
-    array = np.array([1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0])
-    expected_gini = 0.23140495867768596
+    # asserting char array with max impurity
+    array = np.array([0, 1])
+    expected_gini = 1/2 * 1/2 * 2  # 1/2 * 1/2
+    print(f"expected gini -> {expected_gini}")
     assert gini.gini_impurity(array) == expected_gini
 
     # asserting odd char array with max impurity
     array = np.array(['A', 'B', 'C'])
-    expected_gini = 1/3 * 1/3 * 1/3
+    expected_gini = 1 - 3*(pow(1/3, 2))
+    print(f"expected gini -> {expected_gini}")
     assert gini.gini_impurity(array) == expected_gini
 
 
@@ -29,9 +32,16 @@ def test_bestsplit_num():
     expected_best = 36
     assert bestsplit.get_bestsplit(x, y) == expected_best
 
+    # values based on hw set 1 - ex 1
+    x = np.array([2, 2, 3, 4, 4, 5, 6, 7, 8, 9])
+    y = np.array([0, 0, 0, 1, 1, 1, 0, 2, 2, 2])
+    assert len(x) == len(y)
+    expected_best = 6.5
+    assert bestsplit.get_bestsplit(x, y) == expected_best
+
 
 def test_bestsplit_cat():
-    # values based on homework set 1
+    # values based on hw set 1 - ex 2
     x = np.array(['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'])
     y = np.array([0,    0,   1,   0,   1,   1,   1,   0,    0])
     assert len(x) == len(y)

tree.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
-from gini import gini_impurity
 
 
 def load_dataset(path):
     df = pd.read_csv(path)
-    print(f"Loaded dataframe.\n {df.head()}")
+    print(f"Loaded dataframe.\n {df.info()}\n")
     return df
 
 
@@ -14,12 +13,16 @@ def tree_grow(x, y, nmin: int, minleaf: int, nfeat):
 
 
 def tree_pred():
-    return "tree"
+    return "pred"
 
 
 df = load_dataset("data.csv")
-x = [[]]
-y = []
-
+x = df.loc[:-2]
+y = df["class"]
 print(x, y, "\n")
-print(df)
+
+for column in df:
+    print(f"Col: {column}\n{df[column]}\n")
+
+x = np.array([[]])
+y = np.array([])